3732ce2ad7e05463ad6873403c3ed38089d90e10
Mohamed Ali Touir <MohamedAli.Touir@se.linedata.com>
2019-02-07 13:05:36 +0100

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,43 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var authorizedIPs = [];
+var authorizedUsernames = [];
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	
+	/*
+	var addr = req.connection.remoteAddress;
+	if(authorizedIPs.indexOf(addr) > -1)
+	{
+		res.sendFile(__dirname + '/index.html');
+	}
+	else{
+		console.log('unauthorized address '+addr);
+		res.sendStatus(404);
+	}
+	*/
+	
+	// for tests only
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/username', function(req, res){
+	
+	var addr = req.connection.remoteAddress;
+	var idx = authorizedIPs.indexOf(addr);
+	if(idx > -1)
+	{
+		res.send(authorizedUsernames[idx]);
+	}
+	else
+	{
+		/*
+		console.log('unauthorized address '+addr);
+		res.sendStatus(404);
+		*/
+		res.send('');
+	}
 });
 
 io.on('connection', function(socket){
