aeb2e502752080cfaf570ef2a8f566d505c26030
Mohamed Ali Touir <MohamedAli.Touir@se.linedata.com>
2019-02-08 10:52:56 +0100

    added connect + inscription

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('chatapp');
+
+//db.close();
+
 var authorizedIPs = [];
 var authorizedUsernames = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	
@@ -25,7 +36,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/username', function(req, res){
-	
+	//console.log(' => /username');
 	var addr = req.connection.remoteAddress;
 	var idx = authorizedIPs.indexOf(addr);
 	if(idx > -1)
@@ -42,6 +53,54 @@ app.get('/username', function(req, res){
 	}
 });
 
+app.post('/api/inscription', function(req, res) {
+	console.log(' => /api/inscription');
+	console.log(req.body);
+    var username = req.body.email;
+    var password = req.body.password;
+	var firstname = req.body.firstname;
+	var lastname = req.body.lastname;
+	
+	console.log(username,password);
+	
+	db.run("INSERT INTO users (email, password,firstname,lastname) VALUES (?,?,?,?)",username,password,firstname,lastname, function(err){
+		if(err){
+			console.log(err);
+			res.send("failed");
+		}
+		else{
+			res.send("success");
+		}
+	});
+
+});
+
+app.post('/api/connection', function(req, res) {
+	console.log(' => /api/connection');
+	console.log(req.body);
+    var email = req.body.email;
+    var password = req.body.password;
+	
+	console.log(email,password);
+	
+	db.get('SELECT email,firstname,lastname FROM users WHERE email = ? and password = ?',email,password,function(err, result){
+		if(err){
+			console.log(err);
+			res.send("failed");
+		}
+		else{
+			console.log(result);
+			if(result){
+				res.json(result);
+			}
+			else{
+				res.send("failed");
+			}
+		}
+	});
+	
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
@@ -49,5 +108,6 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function(){
+  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT UNIQUE NOT NULL, password TEXT NOT NULL, firstname TEXT NOT NULL, lastname TEXT NOT NULL)");
   console.log('listening on *:' + port);
 });
